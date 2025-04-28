c639eb4a1dff974159a09cc9603ffe95d227e122
yh1820 <yh1820@users.noreply.github.com>
2025-04-28 12:00:46 +0000

    Automated deployment: f9c134952b1037fafd3f10d9b0bcdbf10f5b5bd8 yh1820/yh-blog-source@f9c134952b1037fafd3f10d9b0bcdbf10f5b5bd8

--- js/search.json.js
+++ js/search.json.js
@@ -21,7 +21,7 @@
     function loadData(success) {
         if (!searchData) {
             var xhr = new XMLHttpRequest();
-            xhr.open('GET', '/content.json', true);
+            xhr.open('GET', '/blog/content.json', true);
             xhr.onload = function() {
                 if (this.status >= 200 && this.status < 300) {
                     var res = JSON.parse(this.response||this.responseText);
@@ -54,7 +54,7 @@
             html = data.map(function(post) {
                 return tpl(searchTpl, {
                     title: post.title,
-                    path: post.path,
+                    path: 'blog/' + post.path,
                     date: new Date(post.date).toLocaleDateString(),
                     tags: post.tags.map(function(tag) {
                         return '<span>' + tag.name + '</span>';
